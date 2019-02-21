12c54b5dc8fc9b83b94b0239219398754d214aa4
Chris James <qui666@gmail.com>
2019-02-21 16:21:23 +0000

    important update for @huckocs to style

--- texasholdem.go
+++ texasholdem.go
@@ -27,7 +27,8 @@ func NewTexasHoldem(alerter BlindAlerter, store PlayerStore) *TexasHoldem {
 func (p *TexasHoldem) Start(numberOfPlayers int, alertsDestination io.Writer) {
 	blindIncrement := time.Duration(5+numberOfPlayers) * time.Minute
 
-	blindTime := 0 * time.Second
+	var blindTime time.Duration
+
 	for _, blind := range blinds {
 		p.alerter.ScheduleAlertAt(blindTime, blind, alertsDestination)
 		blindTime = blindTime + blindIncrement
